03f2c366f3aa63679493c0678c81044739fe28b2
ashaaut <ashaautade1998@gmail.com>
2020-10-24 19:53:42 +0530

    Added style for employee card

--- src/js/components/employee.jsx
+++ src/js/components/employee.jsx
@@ -3,10 +3,6 @@ import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import PhoneIcon from '@material-ui/icons/Phone';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import RoomIcon from '@material-ui/icons/Room';
-import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import WorkIcon from '@material-ui/icons/Work';
 import { MdMoreVert} from "react-icons/md";
 
@@ -19,14 +15,15 @@ class Employee extends Component {
       selectedOption:null
     })
   }
-
+  onClick(){
+    console.log("clicked")
+  }
   
   render() {
     const options = ["Edit", "update", "delete"]
     const data = this.props;
     console.log(data.data["Profile"])
     console.log(this.state.selectedOption)
-    const ITEM_HEIGHT = 48;
     return (
       <div className="card-container">
         <div className="card-image">
@@ -39,7 +36,7 @@ class Employee extends Component {
             {data.data["Name"]}
             {data.data["Surname"]}
             <div className="menu" >
-            <MdMoreVert size="1.5em"/>
+            <MdMoreVert size="1.5em" onClick={this.onClick}/>
 
             </div>
             
